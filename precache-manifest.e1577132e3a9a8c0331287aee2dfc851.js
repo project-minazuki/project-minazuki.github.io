29ce3da4544643df795ec8a1464c674293388c6a
七海 椎名 <ymd45921@163.com>
2020-07-04 06:14:50 +0800

    init：

---
+++ precache-manifest.e1577132e3a9a8c0331287aee2dfc851.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35319423157214af82a8ed2fbdcf7d02",
+    "url": "./Minazuki-BBS-Frontend/index.html"
+  },
+  {
+    "revision": "d41897ba2e1b7534a41a",
+    "url": "./Minazuki-BBS-Frontend/static/css/10.2e2fd2c8.chunk.css"
+  },
+  {
+    "revision": "d4b0e9207a1ba64575b0",
+    "url": "./Minazuki-BBS-Frontend/static/css/11.0fb3545e.chunk.css"
+  },
+  {
+    "revision": "4a85aa01860607db1589",
+    "url": "./Minazuki-BBS-Frontend/static/css/4.3cb76441.chunk.css"
+  },
+  {
+    "revision": "4931b7429d5d5eaa80f3",
+    "url": "./Minazuki-BBS-Frontend/static/css/6.92a92a01.chunk.css"
+  },
+  {
+    "revision": "93df28c71d19223f202b",
+    "url": "./Minazuki-BBS-Frontend/static/css/7.ccd7fb69.chunk.css"
+  },
+  {
+    "revision": "2543d446dcff0d090675",
+    "url": "./Minazuki-BBS-Frontend/static/css/8.5e778209.chunk.css"
+  },
+  {
+    "revision": "e0af52cc2d78a7c6f821",
+    "url": "./Minazuki-BBS-Frontend/static/css/main.bf1d8035.chunk.css"
+  },
+  {
+    "revision": "c8eda56c7533770d4d2f",
+    "url": "./Minazuki-BBS-Frontend/static/js/0.ebda0759.chunk.js"
+  },
+  {
+    "revision": "717f312d84116593af81",
+    "url": "./Minazuki-BBS-Frontend/static/js/1.2cd6e366.chunk.js"
+  },
+  {
+    "revision": "d41897ba2e1b7534a41a",
+    "url": "./Minazuki-BBS-Frontend/static/js/10.a77bcb66.chunk.js"
+  },
+  {
+    "revision": "d4b0e9207a1ba64575b0",
+    "url": "./Minazuki-BBS-Frontend/static/js/11.c24fe633.chunk.js"
+  },
+  {
+    "revision": "0224b6b73f82494f13a9",
+    "url": "./Minazuki-BBS-Frontend/static/js/12.822d07e2.chunk.js"
+  },
+  {
+    "revision": "4a85aa01860607db1589",
+    "url": "./Minazuki-BBS-Frontend/static/js/4.fd579f8f.chunk.js"
+  },
+  {
+    "revision": "908efc0c242a46a807a26bd466ddfd09",
+    "url": "./Minazuki-BBS-Frontend/static/js/4.fd579f8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3bab19fb87b1c606627",
+    "url": "./Minazuki-BBS-Frontend/static/js/5.115db5c0.chunk.js"
+  },
+  {
+    "revision": "4931b7429d5d5eaa80f3",
+    "url": "./Minazuki-BBS-Frontend/static/js/6.7515effd.chunk.js"
+  },
+  {
+    "revision": "93df28c71d19223f202b",
+    "url": "./Minazuki-BBS-Frontend/static/js/7.f6c22834.chunk.js"
+  },
+  {
+    "revision": "2543d446dcff0d090675",
+    "url": "./Minazuki-BBS-Frontend/static/js/8.35c72d19.chunk.js"
+  },
+  {
+    "revision": "dad878fe830414a04c6f",
+    "url": "./Minazuki-BBS-Frontend/static/js/9.4eee3783.chunk.js"
+  },
+  {
+    "revision": "e0af52cc2d78a7c6f821",
+    "url": "./Minazuki-BBS-Frontend/static/js/main.444ca3a3.chunk.js"
+  },
+  {
+    "revision": "591815e7b2efa044b7ea",
+    "url": "./Minazuki-BBS-Frontend/static/js/runtime-main.ecd59e29.js"
+  },
+  {
+    "revision": "0e9c4a55458b02c4d54139be54e85792",
+    "url": "./Minazuki-BBS-Frontend/static/media/bg-atri-2.0e9c4a55.png"
+  },
+  {
+    "revision": "cf743952c0675dfb0576067680aa0b1d",
+    "url": "./Minazuki-BBS-Frontend/static/media/bg-atri.cf743952.png"
+  },
+  {
+    "revision": "029bf6ded559cd127a9e57c4f1f20e1c",
+    "url": "./Minazuki-BBS-Frontend/static/media/bg-ayane.029bf6de.png"
+  },
+  {
+    "revision": "1b367b7ece956bec72347c17181e9046",
+    "url": "./Minazuki-BBS-Frontend/static/media/bg-shiroha.1b367b7e.png"
+  },
+  {
+    "revision": "c536d3f1782257761f061071b5916081",
+    "url": "./Minazuki-BBS-Frontend/static/media/favicon.c536d3f1.svg"
+  }
+]);
